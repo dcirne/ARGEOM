91ca8b1bb8eb903139022e7e75b04f152c7eb050
Dalmo Cirne <dalmo.cirne@gmail.com>
2013-03-21 14:10:31 -0400

    Introduced a small delay before setting the location manager heading orientation.

--- ARGEOM/DCAugmentedRealityViewController.m
+++ ARGEOM/DCAugmentedRealityViewController.m
@@ -211,7 +211,12 @@ static double piOver180;
     _locationManager.delegate = self;
     _locationManager.headingFilter = kCLHeadingFilterNone;
     _locationManager.distanceFilter = kCLDistanceFilterNone;
-    [self updateLocationManagerHeadingOrientation];
+    
+    double delayInSeconds = [UIApplication sharedApplication].statusBarOrientationAnimationDuration + 0.1;;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^{
+        [self updateLocationManagerHeadingOrientation];
+    });
 
     return _locationManager;
 }
